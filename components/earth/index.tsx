3ba81a37fca30f97fed77b308fd874b2eab338d2
petercoco626 <petercoco626@gmail.com>
2023-02-19 23:17:02 +0900

    [feat] 지구.glb 파일 불러오는거 성공

--- components/earth/index.tsx
+++ components/earth/index.tsx
@@ -2,7 +2,8 @@ import { useResize } from "hooks/useResize";
 import { useCallback, useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import styles from "./index.module.css";
 
 export default function Earth() {
@@ -20,6 +21,10 @@ export default function Earth() {
       renderer.setSize(scW, scH);
     }
   }, [renderer]);
+
+  const easeOutCirc = (x: number) => {
+    return Math.sqrt(1 - Math.pow(x - 1, 4));
+  };
 
   useEffect(() => {
     const { current: container } = containerRef;
@@ -41,14 +46,39 @@ export default function Earth() {
       // 카메라는 좀더 공부해봐야함.
       const camera = new THREE.PerspectiveCamera(50, scW / scH, 0.1, 1000);
 
+      // 카메라 위치에 따라서도 안보일 수가 있다.
+      camera.position.set(0, 1, 2);
+
       // cube 생성기.
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh(geometry, material);
-      camera.position.z = 5;
+      // const geometry = new THREE.BoxGeometry(1, 1, 1);
+      // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      // const cube = new THREE.Mesh(geometry, material);
+      // camera.position.z = 5;
+
+      const loader = new GLTFLoader();
+      loader.load(
+        "models/earth.4.glb",
+        function (gltf) {
+          console.log(gltf);
+          const model = gltf.scene;
+          // model.position.set(1, 1, 0);
+          model.scale.set(0.5, 0.5, 0.5);
+          model.rotation.x += 0.01;
+          scene.add(model);
+        },
+        undefined,
+        function (error) {
+          console.error(error);
+        }
+      );
+
+      // light가 없으면 대상이 안보임...
+      const light = new THREE.DirectionalLight(0xfffffff, 1);
+      light.position.set(2, 2, 5);
+      scene.add(light);
 
       // sence에 큐브 추가하기.
-      scene.add(cube);
+      // scene.add(cube);
 
       // 현재 코드에서는 돔 자체를 이동시키는거 같음?
       const controls = new OrbitControls(camera, renderer.domElement);
@@ -56,10 +86,9 @@ export default function Earth() {
       const animate = function () {
         // 정확히 어떤 기능인지 파악 필요.
         requestAnimationFrame(animate);
-
-        cube.rotation.y += 0.01;
         renderer.render(scene, camera);
       };
+
       animate();
 
       return () => {
